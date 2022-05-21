57cdc7fb68a3815f22500307a0e054bb29080dab
Xyza Rivera <xyzarivera@gmail.com>
2022-05-21 10:38:07 +0800

    display colors

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -5,10 +5,15 @@ interface IAppProps {
   onClick: (num: number) => void;
 }
 
+export const defaultDivStyle: React.CSSProperties = {};
+
 const App: React.FunctionComponent<IAppProps> = (props) => {
   const { color, onClick } = props;
   return (
-    <div onClick={() => onClick(9)}>
+    <div
+      onClick={() => onClick(9)}
+      style={{ ...defaultDivStyle, backgroundColor: color }}
+    >
       <p>Im a box? {color}</p>
     </div>
   );
